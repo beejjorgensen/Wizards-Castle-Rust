29434805e51511082e4b4d035b7b54173fbe4e39
Brian "Beej Jorgensen" Hall <beej@beej.us>
2018-12-21 17:57:41 -0800

    monsters

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,82 @@ enum RoomType {
     StairsUp,
     Gold,
     Pool,
-    //Monster(Monster)
+    Monster(Monster)
 }
 
-//#[derive(Debug)]
-//struct Monster {
-//    monster_type: usize
-//}
+const MONSTER_COUNT: usize = 13;
+
+#[derive(Debug,PartialEq)]
+enum MonsterType {
+    Kobold,
+    Orc,
+    Wolf,
+    Goblin,
+    Ogre,
+    Troll,
+    Bear,
+    Minotaur,
+    Gargoyle,
+    Chimera,
+    Balrog,
+    Dragon,
+    Vendor,
+}
+
+#[derive(Debug,PartialEq)]
+struct Monster {
+    monster_type: MonsterType,
+    name: String,
+    hp: usize,
+    damage: usize,
+    break_weapon: bool,
+}
+
+impl Monster {
+    fn new(monster_num: usize) -> Monster {
+        let name = [
+            "kobold",
+            "orc",
+            "wolf",
+            "goblin",
+            "ogre",
+            "troll",
+            "bear",
+            "minotaur",
+            "gargoyle",
+            "chimera",
+            "balrog",
+            "dragon",
+            "vendor",
+        ];
+
+        let monster_type = match monster_num {
+            0 => MonsterType::Kobold,
+            1 => MonsterType::Orc,
+            2 => MonsterType::Wolf,
+            3 => MonsterType::Goblin,
+            4 => MonsterType::Ogre,
+            5 => MonsterType::Troll,
+            6 => MonsterType::Bear,
+            7 => MonsterType::Minotaur,
+            8 => MonsterType::Gargoyle,
+            9 => MonsterType::Chimera,
+            10 => MonsterType::Balrog,
+            11 => MonsterType::Dragon,
+            12 => MonsterType::Vendor,
+            _ => MonsterType::Vendor, // This should never happen
+        };
+
+        let m1 = monster_num + 1; // Change to 1-based
+
+        let hp = m1 + 2;
+        let damage = 1 + m1 / 2;
+
+        let break_weapon = monster_type == MonsterType::Gargoyle || monster_type == MonsterType::Dragon;
+
+        Monster{monster_type, name: String::from(name[monster_num]), hp, damage, break_weapon}
+    }
+}
 
 #[derive(Debug)]
 struct Room {
@@ -34,6 +103,7 @@ struct Dungeon {
 }
 
 impl Dungeon {
+
     fn new(xsize: usize, ysize: usize, zsize:usize) -> Dungeon {
         let mut levels = Vec::new();
 
@@ -44,14 +114,20 @@ impl Dungeon {
         let stair_count = area / 32; // 2 in 8x8
         let item_count = area / 21; // 3 in 8x8
 
+        let entrance_x = (xsize - 1) / 2;
+
         for z in 0..zsize {
             let mut v = Vec::new();
             let mut remaining = area;
 
+            // Entrance
+
             if z == 0 {
-                v.push(Room{ roomtype: RoomType::Entrance, discovered: false});
+                v.push(Room{ roomtype: RoomType::Entrance, discovered: true});
                 remaining -= 1;
             }
+
+            // Stairs down
 
             if z < zsize - 1 {
                 for _ in 0..stair_count {
@@ -60,6 +136,8 @@ impl Dungeon {
                 }
             }
 
+            // Stairs up
+
             if z > 0 {
                 for _ in 0..stair_count {
                     v.push(Room{ roomtype: RoomType::StairsUp, discovered: false});
@@ -67,11 +145,22 @@ impl Dungeon {
                 }
             }
 
+            // Items
+
             for _ in 0..item_count {
                 v.push(Room{ roomtype: RoomType::Gold, discovered: false});
                 v.push(Room{ roomtype: RoomType::Pool, discovered: false});
                 remaining -= 2;
             }
+
+            // Monsters {
+            for i in 0..MONSTER_COUNT {
+                v.push(Room{ roomtype: RoomType::Monster(Monster::new(i)), discovered: false});
+                remaining -= 1;
+
+            }
+
+            // Fill the rest with empty
 
             for _ in 0..remaining {
                 v.push(Room{ roomtype: RoomType::Empty, discovered: false});
@@ -84,11 +173,11 @@ impl Dungeon {
                 for x in 0..xsize {
                     let i = y * xsize + x;
 
-                    let entrance_x: usize;
-                    let entrance_y: usize;
-
-                    // Find the entrance
+                    // Swap the entrance
                     if v[i].roomtype == RoomType::Entrance {
+                        let i2 = 0 * xsize + entrance_x;
+
+                        v.swap(i, i2);
                     }
                 }
             }
@@ -98,6 +187,10 @@ impl Dungeon {
         }
 
         Dungeon{levels, xsize, ysize, zsize}
+    }
+
+    fn entrance_x(&self) -> usize {
+        return (self.xsize - 1) / 2;
     }
 
     fn room_at(&self, x: usize, y: usize, z: usize) -> &Room { // TODO: Result
@@ -144,7 +237,7 @@ impl Dungeon {
                         RoomType::StairsUp => print!("U"),
                         RoomType::Gold => print!("G"),
                         RoomType::Pool => print!("P"),
-                        //RoomType::Monster(_) => print!("M"),
+                        RoomType::Monster(_) => print!("M"),
                     }
                 } else {
                     print!("?");
@@ -183,7 +276,7 @@ impl Player {
 
 fn main() {
     let dungeon = Dungeon::new(8, 8, 8);
-    let player = Player::new(3, 0, 0);
+    let player = Player::new(dungeon.entrance_x(), 0, 0);
 
     dungeon.map_dump(player);
     //dungeon.map(player);
