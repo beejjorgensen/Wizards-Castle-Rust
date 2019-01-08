c5e5e037c0264ac856cb71268d307a7a87ee2075
Brian "Beej Jorgensen" Hall <beej@beej.us>
2019-01-07 23:48:54 -0800

    turn off map debugging

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1449,7 +1449,7 @@ fn main() {
 
                     match command.get(..1) {
                         Some("M") => {
-                            ui.map(true);
+                            ui.map(false);
                             print_stats = false;
                             resolve_room_effects = false;
                         },
